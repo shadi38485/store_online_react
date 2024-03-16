117fb10fd415973e1b4a73311b285abd5c922a29
shadi38485 <shadisalehi38485@gmail.com>
2024-03-16 08:24:26 +0100

    use states in react

--- src/components/ProductList/ProductCard.jsx
+++ src/components/ProductList/ProductCard.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const ProductCard = ({img, title, price}) => {
+const ProductCard = ({ id, img, title, price }) => {
+  // let qty = 0;
+  const [qty, setQty]= useState(0)
+  const [bg, setBg] = useState("bg-green")
+
+  // const fn = "Zahra"
+  // const ln = "Yazdani"
+  // const age = 34
+
+  // const result= "I'm "+ fn + " "+ ln + ".I'm "+ age+"!!"
+  // const result2= `I'm ${fn ? "Admin": fn} ${ln}. I'm ${age}!! `
+
+
+  const addToCard = () => {
+    // qty++;// qty= qty+1
+    setQty(qty+1)
+    console.log(qty);
+  };
+
   return (
-    <div className="product-card">
-      <img
-        src={img}
-        alt=""
-      />
+    <div className={`product-card ${bg}`} id={id}>
+      <img src={img} alt="" />
       <div className="product-properties">
         <h3>{title}</h3>
-        <p className='product-price'>{price}$</p>
+        <p className="product-price">{price}$</p>
+      </div>
+      <div className="product-card-footer">
+        <button className="" onClick={addToCard}>
+          Add To Card
+        </button>
+        <span>{qty}</span>
       </div>
     </div>
   );
-}
+};
 
-export default ProductCard
+export default ProductCard;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,25 +3,43 @@ import "./ProductList.css"
 import ProductCard from './ProductCard';
 
 const ProductList = () => {
+
+   const productsData = [
+     {
+       id: 1,
+       image:
+         "https://m.media-amazon.com/images/I/21--f3wS6SL._AC_UF226,226_FMjpg_.jpg",
+       title: "Hedphone",
+       price: 1000,
+     },
+     {
+       id: 2,
+       image:
+         "https://m.media-amazon.com/images/I/81S9AlFMHaL._AC_UF226,226_FMjpg_.jpg",
+       title: "T-shirt",
+       price: 400,
+     },
+     {
+       id: 3,
+       image:
+         "https://m.media-amazon.com/images/I/41UPo0WJwvL._AC_UF226,226_FMjpg_.jpg",
+       title: "Watch",
+       price: 1500,
+     },
+   ];
+
+
   return (
     <div className="product-list">
-      <ProductCard
-        img="https://m.media-amazon.com/images/I/21--f3wS6SL._AC_UF226,226_FMjpg_.jpg"
-        title="Hedphone"
-        price="1000"
-      />
-      <ProductCard
-        img="https://m.media-amazon.com/images/I/81S9AlFMHaL._AC_UF226,226_FMjpg_.jpg"
-        title="T-shirt"
-        price="400"
-      />
-      <ProductCard
-        img="https://m.media-amazon.com/images/I/41UPo0WJwvL._AC_UF226,226_FMjpg_.jpg"
-        title="Watch"
-        price="1600"
-      />
-
-    
+      {productsData.map((item) => (
+        <ProductCard
+          key={item.id}
+          id={item.id}
+          img={item.image}
+          title={item.title}
+          price={item.price}
+        />
+      ))}
     </div>
   );
 }
